2d32fb937d0ab9cc68bda27e9400281113ff4e56
RoxanaBulai15 <roxanabulai73@gmail.com>
2020-10-21 15:59:06 +0300

    Solutie ptr fisierul advanced.txt

---
+++ Day1/advanced.py
@@ -0,0 +1,31 @@
+def dictionary():
+    file = open("advanced.txt", "r")
+    #am extras textul din file
+    text = file.read()
+    #am facut o lista cu liniile din fisier
+    lines = text.split("\n")
+    #print(lines)
+    #am facut o lista de liste(o lista care contine alte liste cu cheia+valoarea)
+    list = []
+    for index in range(len(lines)):
+        list.append(lines[index].split(":"))
+    #print(list)
+    #parcurg lista mare si formez o noua lista doar cu liste ce contin 2 elemente: cheie+valoare
+    newlist = []
+    for index in range(len(list)):
+        if len(list[index]) == 2:
+            newlist.append(list[index])
+    #print(newlist)
+
+    dictionar = {}
+    for i in range(len(newlist)):
+        key = newlist[i][0]
+        value = newlist[i][1]
+        dictionar[key] = value
+    print("Dictionarul format are urmatoarele key cu valori: ")
+    for key, value in dictionar.items():
+        print("Key: " + key + "," + "Value: " + value)
+
+
+
+dictionary()
