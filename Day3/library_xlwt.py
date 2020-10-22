40b750b84d7db06cd12ab85d29eaab812a7bedd9
RoxanaBulai15 <72975669+RoxanaBulai15@users.noreply.github.com>
2020-10-22 18:24:53 +0300

    Add files via upload

---
+++ Day3/library_xlwt.py
@@ -0,0 +1,82 @@
+import xlwt
+from xlwt import Workbook
+
+wb = xlwt.Workbook()
+sheet1 = wb.add_sheet("advanced")
+font0 = xlwt.Font()
+font0.name = "Times New Roman"
+font0.colour_index = 2
+font0.bold = True
+
+style1 = xlwt.XFStyle()
+style1.font = font0
+
+style1 = xlwt.easyxf('pattern: pattern solid, fore_colour yellow;')
+
+first_col = sheet1.col(0)
+first_col.width = 256 * 20  # 20 characters wide (-ish)
+
+
+second_col = sheet1.col(1)
+second_col.width = 256 * 70
+
+sheet1.write(0, 0, "KEY", style1)
+sheet1.write(0, 1, "VALUE", style1)
+
+def dictionary():
+    file = open("advanced.txt", "r")
+    # am extras textul din file
+    text = file.read()
+    # am facut o lista cu liniile din fisier
+    lines = text.split("\n")
+    # print(lines)
+    # am facut o lista de liste(o lista care contine alte liste cu cheia+valoarea)
+    list = []
+    for index in range(len(lines)):
+        list.append(lines[index].split(":"))
+    # print(list)
+    # parcurg lista mare si formez o noua lista doar cu liste ce contin 2 elemente: cheie+valoare
+    newlist = []
+    for index in range(len(list)):
+        if len(list[index]) >= 2:
+            newlist.append(list[index])
+    # print(newlist)
+
+    dictionar = {}
+    for i in range(len(newlist)):
+        key = newlist[i][0]
+        value = newlist[i][1]
+        if len(newlist[i]) > 1:
+            for j in range(2, len(newlist[i])):
+                value = value + ":" + newlist[i][j]
+        dictionar[key] = value
+    print("Dictionarul format are urmatoarele key cu valori: ")
+    row = 1
+    for key, value in dictionar.items():
+        column = 0
+        sheet1.write(row,column, key)
+        column = column + 1
+        sheet1.write(row, column, value)
+        row = row +1
+
+    wb.save("advanced.xlsx")
+
+
+dictionary()
+
+
+#file1 = open('advanced.txt', 'r')
+#lines = file1.readlines()
+
+#dictionary = {}
+#for i in lines:
+    #split = i.split(":", 1)
+    #if len(split) > 1:
+        #dictionary[split[0]] = split[1].replace("\n", "")
+    #else:
+        #print("The ", split, " was excluded")
+
+#count = 0
+#for key, value in dictionary.items():
+    #count += 1
+    #print(count, "  Key: " + key + "," + "Value: " + value)
