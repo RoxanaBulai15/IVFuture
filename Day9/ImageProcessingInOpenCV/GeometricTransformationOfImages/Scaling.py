85eb4eb4492d5a2604720448c99cef8e0a4b9ddb
RoxanaBulai15 <roxanabulai73@gmail.com>
2020-11-02 11:50:47 +0200

    Ziua9

---
+++ Day9/ImageProcessingInOpenCV/GeometricTransformationOfImages/Scaling.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2 as cv
+img = cv.imread('D:\IVFuture\opencv\doc\images\opticalfb.jpg')
+#res = cv.resize(img,None,fx=2, fy=2, interpolation = cv.INTER_CUBIC)
+#OR
+height, width = img.shape[:2]
+res = cv.resize(img,(2*width, 2*height), interpolation = cv.INTER_CUBIC)
+
+cv.imshow('img',res)
+cv.waitKey(0)
