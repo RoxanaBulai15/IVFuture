0b874fd444e67aedbd2e1d0f01af16148202a21f
RoxanaBulai15 <roxanabulai73@gmail.com>
2020-11-07 10:10:44 +0200

    Modificare

--- Day9/ImageProcessingInOpenCV/SmoothingImages/ImageBlurring/AVERAGING.py
+++ Day9/ImageProcessingInOpenCV/SmoothingImages/ImageBlurring/AVERAGING.py
@@ -1,3 +1,4 @@
+import cv2
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
@@ -8,7 +9,8 @@ from matplotlib import pyplot as plt
 # This is done by the function cv.blur() or cv.boxFilter()
 
 
-img = cv.imread('/opencv/doc/images/opticalfb.jpg')
+#img = cv.imread('/opencv/doc/images/opticalfb.jpg')
+img= cv2.imread('dog.jpg')
 blur = cv.blur(img,(5,5))
 plt.subplot(121),plt.imshow(img),plt.title('Original')
 plt.xticks([]), plt.yticks([])
