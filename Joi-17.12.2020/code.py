1e41ee6eaae0065ae9569ed4b754a5eb27c03b1f
Adrian <adrian.tesula@ivfuture.uk>
2020-12-17 15:29:21 +0200

    Change from scratch to class

--- Joi-17.12.2020/code.py
+++ Joi-17.12.2020/code.py
@@ -1,27 +1,26 @@
 import json
 
-x = input("User: ")
 
-with open('chatbot.json') as json_file:
-    data = json.load(json_file)
-    #for p in data['chatbot']:
-    #    print('Tag: ' + p['tag'])
-    #    for i in p['patterns']:
-    #        print("Patterns: " + i)
-    #    for j in p['responses']:
-    #        print("Responses: " + j)
-    # print('\n')
-    ok=0
-    while ok==0:
-        for p in data['chatbot']:
-            for i in p['patterns']:
-                if i==x:
-                    for j in p['responses']:
-                        print("Chatbot: "+ j)
-                        ok=1
-                        break
-        if ok==0:
-            x=input("Chatbot: Invalid answer. Enter again: ")
+class ChatBot:
+    def __init__(self, userAnswer):
+        self.userAnswer = userAnswer
+        with open('chatbot.json') as json_file:
+            self.data = json.load(json_file)
+
+    def check_answer(self):
+        print("Here")
+        while True:
+            for p in self.data['chatbot']:
+                for i in p['patterns']:
+                    if i == self.userAnswer:
+                        for j in p['responses']:
+                            print("Chatbot: " + j)
+                            break
+            # if ok == 0:
+            #     x = input("Chatbot: Invalid answer. Enter again: ")
 
 
-
+if __name__ == '__main__':
+    userAnswer = input("User: ")
+    chatBot = ChatBot(userAnswer)
+    chatBot.check_answer()
