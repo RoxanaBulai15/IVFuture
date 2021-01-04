f380d0c97efde86961c64b5397c8a2aeee7ef3d3
RoxanaBulai15 <roxanabulai73@gmail.com>
2021-01-04 16:41:41 +0200

    Task2

--- ChatbotProject/site_for_chatbot.py
+++ ChatbotProject/site_for_chatbot.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import chatbot as mymodule
 
 app = Flask(__name__)
-
+app.static_folder='static'
 
 @app.route('/')
 def home():
@@ -10,17 +10,19 @@ def home():
 
 
 @app.route('/get')
-def get_bot_response(userAnswer):
+def get_bot_response():
+    userAnswer = request.args.get('msg')
     chat = mymodule.ChatBot(userAnswer)
+    print(str(chat.check_answer()))
     return str(chat.check_answer())
 
 
 if __name__ == '__main__':
-    userAnswer = input("User: ")
-    chat = mymodule.ChatBot(userAnswer)
+    #userAnswer = input("User: ")
+    #chat = mymodule.ChatBot(userAnswer)
     #chat.check_answer();
     #get_bot_response(chat)
     #chat.check_answer()
-    chat.printForSiteMethod()
+    #chat.printForSiteMethod()
 
     app.run()
